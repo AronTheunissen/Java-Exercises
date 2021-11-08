46d763e0fc6627cf339f8494a6d70ac9331e0490
Aron Theunissen <atheunissen@sogyo.nl>
2021-11-08 10:35:37 +0100

    Made Quote a running program as the assignment stated, ready for feedback.

--- src/main/java/nl/sogyo/javaopdrachten/quote/Quote.java
+++ src/main/java/nl/sogyo/javaopdrachten/quote/Quote.java
@@ -1,4 +1,7 @@
 package nl.sogyo.javaopdrachten.quote;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.time.*;
 
 public class Quote {
     String[][] quotes = {
@@ -11,6 +14,33 @@ public class Quote {
     };
 
     public static void main(String... args) {
-        //TODO
+		Quote quote = new Quote();
+        LocalDate date = LocalDate.now();
+		int dayNumber = date.getDayOfYear();
+		int quoteNumber = (dayNumber + 5) % 6;
+		String[] quoteOfDay = quote.quotes[quoteNumber];
+		String writer = quoteOfDay[0];
+		String justQuote = quoteOfDay[1];
+		
+		String[] nameParts = writer.split("\s");
+		String capitalWriter = "";
+		for(int i = 0; i < nameParts.length; i++){
+			nameParts[i] = nameParts[i].substring(0,1).toUpperCase() + nameParts[i].substring(1);
+			capitalWriter = capitalWriter + " " + nameParts[i];
+		}
+		
+		justQuote = justQuote.substring(0,1).toUpperCase() + justQuote.substring(1);
+		char[] quoteLetters = justQuote.toCharArray();
+		if(quoteLetters[quoteLetters.length-1] != '!' && quoteLetters[quoteLetters.length-1] != '?' && quoteLetters[quoteLetters.length-1] != '.'){
+			justQuote = justQuote + ".";
+		}
+		System.out.println("The quote for " + date + " : " + justQuote + " -- " + capitalWriter);
     }
 }
+
+
+
+
+
+
+
